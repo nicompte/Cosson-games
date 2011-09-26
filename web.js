7ef5eba44cfaf6584c1d9be65f5d2ec590d0a40a
nicompte <nicotoutcourt@hotmail.com>
2011-09-26 21:31:29 +0000

    ajout de stylus

--- web.js
+++ web.js
@@ -1,10 +1,21 @@
 var port = process.env.C9_PORT;
 var express = require("express");
+var stylus = require("stylus");
 
 var app = express.createServer();
 
+app.configure(function() {
+  app.set ('view engine', 'jade');
+  app.use( stylus.middleware({
+    src: __dirname + "/views",
+    dest: __dirname + "/public",
+    compress: true
+  }));
+  app.use (express.static(__dirname + '/public'));
+});
+
 app.get('/belote/:template', function(req, res){
-    res.render(template+'/index.jade', { title: 'My Site' });
+    res.render(req.params.template+'/index', { title: 'My Site' });
 });
 
 app.listen(port);
