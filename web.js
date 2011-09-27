81c8f88cd1c255047f9469e5c24e4c45aef72fb4
nicompte <nicotoutcourt@hotmail.com>
2011-09-27 05:39:00 +0000

    Mise en place socket.io

--- web.js
+++ web.js
@@ -1,8 +1,9 @@
 var port = process.env.C9_PORT;
 var express = require("express");
 var stylus = require("stylus");
+var io = require("socket.io");
 
-var app = express.createServer();
+var app = express.createServer(), io = io.listen(app);
 
 app.configure(function() {
   app.set ('view engine', 'jade');
@@ -16,6 +17,13 @@ app.configure(function() {
 
 app.get('/belote/:template', function(req, res){
     res.render(req.params.template+'/index', { title: 'My Site' });
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
 
 app.listen(port);
