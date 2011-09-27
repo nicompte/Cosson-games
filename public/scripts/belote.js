6ae1ce30b10b42d6daf441037f99ca82acfcc7cb
nicompte <nicotoutcourt@hotmail.com>
2011-09-27 16:55:49 +0000

    Ajout sessions

--- public/scripts/belote.js
+++ public/scripts/belote.js
@@ -1,5 +1,25 @@
-var socket = io.connect('http://cosson-games.nicompte.c9.io');
+var socket = io.connect('http://cosson-games.nicompte.c9.io/belote');
 socket.on('news', function (data) {
     console.log(data);
     socket.emit('my other event', { my: 'data' });
 });
+
+socket.on('error', function (reason){
+  console.error('Unable to connect Socket.IO', reason);
+});
+
+socket.on('connect', function (){
+  console.info('successfully established a working connection');
+});
+
+/*
+var sio = io.connect();
+
+sio.socket.on('error', function (reason){
+  console.error('Unable to connect Socket.IO', reason);
+});
+
+sio.on('connect', function (){
+  console.info('successfully established a working connection \o/');
+});
+*/
--- web.js
+++ web.js
@@ -1,7 +1,10 @@
 var port = process.env.C9_PORT, 
 express = require("express"), 
 stylus = require("stylus"), 
-io = require("socket.io");
+io = require("socket.io"),
+connections = {};
+//const parseCookie = require('connect').utils.parseCookie;
+
 
 //var deckModel = require("./models/Deck.js").Deck();
 
@@ -16,7 +19,30 @@ app.configure(function() {
     dest: __dirname + "/public",
     compress: true
   }));
+  /*
+  this.sessionStore = new express.session.MemoryStore({ reapInterval: 60000 * 10 });
+  this.use(express.session({
+    "secret": "some private string",
+    "store":  this.sessionStore                                            
+  }));
+  */
   app.use (express.static(__dirname + '/public'));
+});
+
+/* Middleware for limited access */
+/*
+function requireLogin (req, res, next) {
+  if (req.session.username) {
+    next();
+  } else {
+    res.redirect("/");
+  }
+}
+*/
+ 
+/* Home page (requires authentication) */
+app.get('/secured', /*[requireLogin],*/ function (req, res/*, next*/) {
+   res.send('Accès sécurisé');
 });
 
 app.get('/', function(req, res){
@@ -26,12 +52,58 @@ app.get('/', function(req, res){
 app.get('/belote/:template', function(req, res){
     res.render(req.params.template+'/index', { title: 'Cosson games' });
 });
-
-io.sockets.on('connection', function (socket) {
+/*
+io.of('/belote').on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
   });
 });
 
+io.configure(function (){
+    io.set('authorization', function (handshakeData, callback) {
+      var cookies = parseCookie(handshakeData.headers.cookie);
+      //console.log(cookies);
+      var sessionID = null;//cookies['connect.sid'];
+      if (!sessionID) {
+        callback('No session', false);
+      } else {
+        handshakeData.sessionID = sessionID;
+        app.sessionStore.get(sessionID, function (err, session) {
+          if (!err && session && session.username) {
+            handshakeData.username = session.username;
+            callback(null, true);
+          } else {
+            callback(err || 'User not authenticated', false);
+          }
+        });
+      }
+    });
+});
+
+
+io.on('connection', function (socket) { 
+  var sessionID = socket.handshake.sessionID;
+  var username = socket.handshake.username;
+  if ('undefined' == typeof connections[sessionID]) {
+    connections[sessionID] = { "length": 0 };
+    io.emit('join', username, Date.now());
+  }
+  connections[sessionID][socket.id] = socket;
+  connections[sessionID].length ++;
+});
+
+io.on('disconnect', function () {
+    var userConnections = connections[sessionID];
+    if (userConnections.length && userConnections[socket.id]) {
+      userConnections.length --;
+      delete userConnections[socket.id];
+    }
+    if (userConnections.length === 0) {
+      io.emit('bye', username, Date.now());
+    }
+});
+*/
+
 app.listen(port);
+//http://naholyr.fr/2011/07/authentification-et-websocket-avec-node-js-express-et-socket-io/
